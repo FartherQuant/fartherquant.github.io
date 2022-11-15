cf0eae0bc454b884a641fc08f41e25a79b4bd6e4
FartherQuant <118244927+FartherQuant@users.noreply.github.com>
2022-11-15 13:57:09 +0800

    init

---
+++ assets/createSeriesDataSimply.187aa19b.js
@@ -0,0 +1,1 @@
+import{i as t,g as c,aT as m,aU as o}from"./index.esm.min.143e7a13.js";function S(e,a,n){a=t(a)&&{coordDimensions:a}||c({encodeDefine:e.getEncode()},a);var r=e.getSource(),s=m(r,a).dimensions,i=new o(s,e);return i.initData(r,n),i}export{S as c};
